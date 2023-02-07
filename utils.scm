e81a8fc43af32248d9d47e09c4d452e0f00ea038
XiangyuGuo <tobielf@gmail.com>
2023-02-07 04:38:04 -0700

    Add utility functions.
    To solve predicate logic, we need to add a set of utility functions

--- utils.scm
+++ utils.scm
@@ -19,3 +19,48 @@
 
 (define (set-value record value)
   (set-cdr! record value))
+
+;;; Get first n elements from a list.
+; O(n) complexity.
+(define (get-first-n-elements l n)
+  (if (or (= n 0) (null? l))
+    `()
+    (cons (car l) (get-first-n-elements (cdr l) (- n 1)))))
+
+;;; Find bounded variables from a substitution.
+; O(n) complexity.
+(define (find-bound-vars argv S)
+  (if (null? argv)
+      '()
+      (let ((key (walk (car argv) S)))
+        (if (not (var? key))
+            (cons (car argv) (find-bound-vars (cdr argv) S))
+            (find-bound-vars (cdr argv) S)))))
+
+;;; Extend a substitution with <variable, value> pairs.
+; O(n) complexity, where n is the length of <variable, value> list.
+(define (ext-s-for-all-vars vars values S)
+  (if (null? vars)
+      S
+      (ext-s-for-all-vars 
+          (cdr vars) 
+          (cdr values) 
+          (ext-s (car vars) (car values) S))
+))
+
+;;; Check if a variable is in a list of variables.
+; O(n) complexity.
+(define (contain? var var-to-remove)
+  (cond ((null? var-to-remove) #f)
+        ((eq? var (car var-to-remove)) #t)
+        (else (contain? var (cdr var-to-remove)))))
+
+;;; Remove a list of variables from a given removal list.
+; O(n^2) complexity.
+(define (remove-var-from-list var-list var-to-remove)
+  (cond ((null? var-list) `())
+        ((contain? (car var-list) var-to-remove) 
+          (remove-var-from-list (cdr var-list) var-to-remove))
+        (else 
+          (cons (car var-list) 
+                (remove-var-from-list (cdr var-list) var-to-remove)))))
